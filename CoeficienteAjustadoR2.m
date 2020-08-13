663f1dfae48d5d897496fd19fa40a39d1ed47fbc
Raíssa Ellen de Sousa <44507419+Raissa016@users.noreply.github.com>
2020-08-13 16:01:07 -0300

    Update CoeficienteAjustadoR2.m

--- CoeficienteAjustadoR2.m
+++ CoeficienteAjustadoR2.m
@@ -1,5 +1,4 @@
-%% COEFICIENTE DE DETERMINA«√O AJUSTADO (R2aj)
-
+%% COEFICIENTE DE DETERMINA√á√ÉO AJUSTADO (R2aj)
 function [ r2aj ] = CoeficienteAjustadoR2( y , ychap , X )
 
 % Determinamos os valores de n , k e p.
@@ -8,5 +7,4 @@ p = k + 1;
 
 % Sabendo que R2aj = 1 - (SQE/(n-p))(Syy/(n-1)) temos:
 r2aj = 1 - (sum((y - ychap).^2) / ( n - p))/(sum((y - mean(y)).^2)/( n - 1));
-
 end
